84b56f86749522b7ff9b4ff489dc447abbd3f5e4
Shraddhesh Pednekar <pednekarshriddhesh010@gmail.com>
2025-09-28 19:25:13 +0530

    This program takes two numbers from the user and shows results of:
    Addition (+)
    Subtraction (-)
    Multiplication (*)
    Division (/)
    Modulus (%)

---
+++ 3.Opretors/Opretor-Solve-Assignment/ArithmeticOperations.java
@@ -0,0 +1,27 @@
+// Write a java program to enter two numbers and perform all arithmetic operations. 
+
+
+import java.util.Scanner;
+
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input two numbers
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        // Use Opretor And  
+        System.out.println("\n--- Results ---");
+        System.out.println("Addition: " + (num1 + num2));
+        System.out.println("Subtraction: " + (num1 - num2));
+        System.out.println("Multiplication: " + (num1 * num2));
+        System.out.println("Division: " + (num1 / num2));
+        System.out.println("Modulus: " + (num1 % num2));
+
+    }
+}
+
