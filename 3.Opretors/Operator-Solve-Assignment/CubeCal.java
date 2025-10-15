ac7c0ad490441f3cc27b611250007f37cacc64bc
Shraddhesh Pednekar <pednekarshriddhesh010@gmail.com>
2025-10-15 15:03:24 +0530

    Add CubeCalculator.java to calculate the cube of a number

---
+++ 3.Opretors/Operator-Solve-Assignment/CubeCal.java
@@ -0,0 +1,18 @@
+//  Write a Java program that reads a number and display the cube.
+
+import java.util.Scanner;
+
+public class CubeCal {
+    public static void main(String x [])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter The Number.");
+        int no = sc.nextInt();
+
+        int cube;
+        cube = no * no * no;
+
+        System.out.println("Cube Of -- "+no+ " -- Is --> "+cube);
+    }
+}
